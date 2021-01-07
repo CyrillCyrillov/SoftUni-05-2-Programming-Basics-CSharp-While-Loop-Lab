1aa207756060197f46f4400b8661bcad74594e8d
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:09:35 +0200

    Add files via upload

---
+++ Task06_Max_Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task06_Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = int.MinValue;
+            while (true)
+            {
+                string nextNumber = Console.ReadLine();
+                if (nextNumber == "Stop")
+                {
+                    break;
+                }
+                int nextNumberDigit = int.Parse(nextNumber);
+                if (nextNumberDigit > maxNumber)
+                {
+                    maxNumber = nextNumberDigit;
+                }
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
