27a6bf9779e67a302ebfa165e36ca91e7c7be160
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:05:40 +0200

    Add files via upload

---
+++ Task02_Password/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task02_Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = Console.ReadLine();
+            string passWord = Console.ReadLine();
+            string input = " ";
+            while (input != passWord)
+            {
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Welcome {userName}!");
+        }
+    }
+}
