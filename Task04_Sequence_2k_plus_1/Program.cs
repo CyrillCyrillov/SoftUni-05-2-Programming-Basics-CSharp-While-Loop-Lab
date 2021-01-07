4ccac73c3034d722f9e426b9e7a90236a136483a
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:07:33 +0200

    Add files via upload

---
+++ Task04_Sequence_2k_plus_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task04_Sequence_2k_plus_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int nextNumber = 1;
+            while (nextNumber <= n)
+            {
+                Console.WriteLine(nextNumber);
+                nextNumber = nextNumber * 2 + 1;
+            }
+        }
+    }
+}
