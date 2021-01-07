a826ca712d9f5aca6b1181cff1750fe385517ba2
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:06:29 +0200

    Add files via upload

---
+++ Task03_Sum_Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task03_Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int upLine = int.Parse(Console.ReadLine());
+            int nextNumber = int.MinValue;
+            int sum = 0;
+            while (sum < upLine)
+            {
+                nextNumber = int.Parse(Console.ReadLine());
+                sum += nextNumber;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
