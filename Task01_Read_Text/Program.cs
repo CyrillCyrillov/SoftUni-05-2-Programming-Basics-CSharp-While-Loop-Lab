7f40e35f35b243a276009e2c7ab4f473492d4ae9
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:04:36 +0200

    Add files via upload

---
+++ Task01_Read_Text/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task01_Read_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string name = Console.ReadLine();
+                if (name == "Stop")
+                {
+                    break;
+                }
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
