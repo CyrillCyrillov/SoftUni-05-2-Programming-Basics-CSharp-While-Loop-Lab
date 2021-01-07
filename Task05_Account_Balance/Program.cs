07faa6e2104b83c18c4c0e838be35043ccbb805d
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:08:39 +0200

    Add files via upload

---
+++ Task05_Account_Balance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task05_Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalSum = 0;
+            while (true)
+            {
+                string nextIncrease = Console.ReadLine();
+                if (nextIncrease == "NoMoreMoney")
+                {
+                    break;
+                }
+                double nextSum = double.Parse(nextIncrease);
+                if (nextSum < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {nextSum:F2}");
+                totalSum += nextSum;
+            }
+            
+            Console.WriteLine($"Total: {totalSum:F2}");
+        }
+    }
+}
