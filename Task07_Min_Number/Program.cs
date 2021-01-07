a49a30f0e495558f1ec598a011baf85ec57410fc
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-07 20:10:27 +0200

    Add files via upload

---
+++ Task07_Min_Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task07_Min_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = int.MaxValue;
+            while (true)
+            {
+                string nextNumber = Console.ReadLine();
+                if (nextNumber == "Stop")
+                {
+                    break;
+                }
+                int nextNumberDigit = int.Parse(nextNumber);
+                if (nextNumberDigit < minNumber)
+                {
+                    minNumber = nextNumberDigit;
+                }
+            }
+            Console.WriteLine(minNumber);
+        }
+    }
+}
